300e31f2922895dcf6df6beaa64c7dfc77799dd1
Alain Afonso <alainpedro123@gmail.com>
2019-05-25 17:05:59 +0200

    Updates

---
+++ precache-manifest.a313b982fda30d49d8c3d8f6c44f418d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "18136dcbef5e1bc114f1",
+    "url": "/robots-family/static/js/runtime~main.472efce2.js"
+  },
+  {
+    "revision": "12b540e33d9747c4d988",
+    "url": "/robots-family/static/js/main.886ce169.chunk.js"
+  },
+  {
+    "revision": "14b757a22cca86ee9f86",
+    "url": "/robots-family/static/js/2.78c198a2.chunk.js"
+  },
+  {
+    "revision": "12b540e33d9747c4d988",
+    "url": "/robots-family/static/css/main.9366054f.chunk.css"
+  },
+  {
+    "revision": "14b757a22cca86ee9f86",
+    "url": "/robots-family/static/css/2.4fdad87f.chunk.css"
+  },
+  {
+    "revision": "a4e7a187907570438a09b37301eecf38",
+    "url": "/robots-family/index.html"
+  }
+];
